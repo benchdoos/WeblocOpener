3833fd08bc6151ab449b663d767fecd21c284233
doos <doos-doos@mail.ru>
2016-11-14 19:56:39 +0300

    Added ApplicationConstants

---
+++ UpdateModule/src/com/doos/core/ApplicationConstants.java
@@ -0,0 +1,10 @@
+package com.doos.core;
+
+/**
+ * Created by Eugene Zrazhevsky on 14.11.2016.
+ */
+public class ApplicationConstants {
+    final public static String UPDATE_ACTIVE = "UPDATE_ACTIVE";
+    final public static String LAST_UPDATED = "LAST_UPDATED";
+    final public static String CURRENT_APP_VERSION = "CURRENT_APP_VERSION";
+}
