f5c65b5e410ef553eb511e31bf990af1c6c040ef
doos <doos-doos@mail.ru>
2016-11-21 21:25:40 +0300

    Updated SettingsDialog

--- src/com/doos/gui/SettingsDialog.java
+++ src/com/doos/gui/SettingsDialog.java
@@ -1,18 +1,18 @@
 package com.doos.gui;
 
-import com.doos.ApplicationConstants;
 import com.doos.Main;
 import com.doos.utils.FrameUtils;
+import com.doos.utils.registry.RegistryCanNotReadInfoException;
+import com.doos.utils.registry.RegistryException;
 import com.doos.utils.registry.RegistryManager;
 import org.apache.log4j.Logger;
 
 import javax.swing.*;
 import java.awt.event.*;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import static com.doos.Main.*;
+import static com.doos.Main.properties;
 import static com.doos.service.Logging.getCurrentClassName;
 
 public class SettingsDialog extends JFrame {
@@ -22,7 +22,6 @@ public class SettingsDialog extends JFrame {
     private JButton buttonOK;
     private JButton buttonCancel;
     private JCheckBox autoUpdateEnabledCheckBox;
-    private JLabel lastUpdateDateLable;
     private JButton updateNowButton;
 
     public SettingsDialog() {
@@ -72,10 +71,16 @@ public class SettingsDialog extends JFrame {
     }
 
     private void onUpdateNow() {
-        /*final File JAR_FILE = new File(SettingsDialog.class.getProtectionDomain()
-                .getCodeSource().getLocation().getPath());*/
-        String run = "java -jar \"" + RegistryManager.getInstallLocationValue()
-                + File.separator + "Updater.jar\"";
+        String run = null;
+        try {
+            run = "java -jar \"" + RegistryManager.getInstallLocationValue()
+                    + File.separator + "Updater.jar\"";
+
+        } catch (RegistryCanNotReadInfoException e) {
+            e.printStackTrace();
+            run = new File(SettingsDialog.class.getProtectionDomain()
+                    .getCodeSource().getLocation().getPath()).getAbsolutePath().replace("%20", " ");
+        }
         System.out.println(">>>> " + run);
         try {
             Runtime.getRuntime().exec(run);
@@ -86,30 +91,23 @@ public class SettingsDialog extends JFrame {
     }
 
     private void loadSettings() {
-        log.info("Loading settings from: " + ApplicationConstants.SETTINGS_FILE_PATH);
         try {
             Main.loadProperties();
-            autoUpdateEnabledCheckBox.setSelected(properties.getProperty(UPDATE_ACTIVE).equals("true"));
-            lastUpdateDateLable.setText(properties.getProperty(LAST_UPDATED));
-        } catch (FileNotFoundException e) {
-            log.warn("Can not load property file: " + ApplicationConstants.SETTINGS_FILE_PATH, e);
-            log.info("Creating a new property file");
-            Main.createNewFileProperties();
-            loadSettings();
-        } catch (IOException e) {
-            e.getStackTrace();
+            autoUpdateEnabledCheckBox.setSelected(Boolean.parseBoolean(properties.getProperty(RegistryManager.KEY_AUTO_UPDATE)));
+        } catch (RegistryException e) {
+            log.warn("Can not load data from registry", e);
+            Main.useDefaultAppProperties(); //To prevent crash
         }
     }
 
 
     private void onOK() {
-        properties.setProperty(UPDATE_ACTIVE, autoUpdateEnabledCheckBox.isSelected() + "");
+        properties.setProperty(RegistryManager.KEY_AUTO_UPDATE, autoUpdateEnabledCheckBox.isSelected() + "");
         try {
-            properties.setProperty(CURRENT_APP_VERSION, ApplicationConstants.APP_VERSION);
             Main.saveProperties();
-        } catch (IOException e) {
-            log.warn("Can not save settings file", e);
-            JOptionPane.showMessageDialog(new JFrame(), "Error", "Can not save settings!\n" + e.getMessage(),
+        } catch (RegistryException e) {
+            log.warn("Can not save settings change", e);
+            JOptionPane.showMessageDialog(new JFrame(), "Error", "Can not save settings to registry.",
                     JOptionPane.ERROR_MESSAGE);
         }
         dispose();
