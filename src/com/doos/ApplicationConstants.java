5405390f0680afc6d9a3ee5943e48a3b8e6f97b6
Eugene Zrazhevsky <eugene.zrazhevsky@gmail.com>
2016-11-22 16:35:39 +0300

    Updated app version

--- src/com/doos/ApplicationConstants.java
+++ src/com/doos/ApplicationConstants.java
@@ -7,7 +7,7 @@ import java.io.File;
  */
 public interface ApplicationConstants {
     String APP_NAME = "WeblocOpener";
-    String APP_VERSION = "1.2";
+    String APP_VERSION = "1.2.1";
     String APP_LOG_FOLDER_PATH = System.getProperty("java.io.tmpdir") + APP_NAME + File.separator + "Log";
     String APP_LOG_PROPERTY = "WeblocOpener.log.folder";
     String SETTINGS_FILE_PATH = System.getProperty("java.io.tmpdir") + APP_NAME + File.separator + "settings.prop";
