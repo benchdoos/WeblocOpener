a4c5acbaaeb260c04a3be09a4690a4b165678391
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2016-11-26 17:19:32 +0300

    code improvements

--- src/com/doos/webloc_opener/service/gui/WindowFocusRequester.java
+++ src/com/doos/webloc_opener/service/gui/WindowFocusRequester.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 
 import static com.doos.webloc_opener.service.Logging.getCurrentClassName;
 
@@ -25,19 +26,24 @@ public class WindowFocusRequester {
     }
 
     public static boolean runScript(String script) {
+        FileWriter fw = null;
         try {
             File file = File.createTempFile("WeblocOpenerScript", ".vbs");
             file.deleteOnExit();
-            FileWriter fw = new java.io.FileWriter(file);
+            fw = new java.io.FileWriter(file);
             log.debug("Temp file created at: " + file.getAbsolutePath());
             fw.write(script);
-            fw.close();
+
             Process p = Runtime.getRuntime().exec("wscript " + file.getPath());
             p.waitFor();
             return (p.exitValue() == 1);
         } catch (Exception e) {
             log.warn("Can not run script: " + script, e);
             return false;
+        } finally {
+            try {
+                fw.close();
+            } catch (IOException ignore) {/*NOP*/}
         }
     }
 }
