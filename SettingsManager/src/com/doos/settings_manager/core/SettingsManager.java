545afa887a8f6682653226e5918f31aa88248091
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2016-12-03 12:28:48 +0300

    Moved some methods to UserUtils

--- SettingsManager/src/com/doos/settings_manager/core/SettingsManager.java
+++ SettingsManager/src/com/doos/settings_manager/core/SettingsManager.java
@@ -5,12 +5,6 @@ import com.doos.settings_manager.registry.RegistryCanNotReadInfoException;
 import com.doos.settings_manager.registry.RegistryCanNotWriteInfoException;
 import com.doos.settings_manager.registry.RegistryManager;
 
-import javax.swing.*;
-import javax.swing.event.HyperlinkEvent;
-import java.awt.*;
-import java.io.IOException;
-import java.net.URI;
-
 /**
  * Created by Eugene Zrazhevsky on 20.11.2016.
  *
@@ -31,39 +25,4 @@ public class SettingsManager {
         }
     }
 
-    public static void showErrorMessageToUser(Component parentComponent, String title, String message) {
-        String msg = "<HTML><BODY><P>" + message + " <br>Please visit " +
-                "<a href=\"" + ApplicationConstants.UPDATE_WEB_URL + "\">" + ApplicationConstants.UPDATE_WEB_URL + "</P></BODY></HTML>";
-        JEditorPane jEditorPane = new JEditorPane();
-        jEditorPane.setContentType("text/html");
-        jEditorPane.setEditable(false);
-        jEditorPane.setHighlighter(null);
-        jEditorPane.setEditable(false);
-        jEditorPane.getCaret().deinstall(jEditorPane);
-        jEditorPane.setBackground(Color.getColor("#EEEEEE"));
-        jEditorPane.addHyperlinkListener(e -> {
-            if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED)) {
-                openAppGithubPage();
-            }
-
-        });
-        jEditorPane.setText(msg);
-        JOptionPane.showMessageDialog(parentComponent,
-                                      jEditorPane,
-                                      "[WeblocOpener] " + title, JOptionPane.ERROR_MESSAGE);
-    }
-
-    private static void openAppGithubPage() {
-        if (!Desktop.isDesktopSupported()) {
-            return;
-        }
-        Desktop desktop = Desktop.getDesktop();
-
-        try {
-            desktop.browse(URI.create(ApplicationConstants.UPDATE_WEB_URL));
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(new Frame(), "URL is corrupt: " + ApplicationConstants.UPDATE_WEB_URL);
-        }
-
-    }
 }
---
+++ SettingsManager/src/com/doos/settings_manager/utils/UserUtils.java
@@ -0,0 +1,50 @@
+package com.doos.settings_manager.utils;
+
+import com.doos.settings_manager.ApplicationConstants;
+
+import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Created by Eugene Zrazhevsky on 03.12.2016.
+ */
+public class UserUtils {
+    public static void showErrorMessageToUser(Component parentComponent, String title, String message) {
+        String msg = "<HTML><BODY><P>" + message + " <br>Please visit " +
+                "<a href=\"" + ApplicationConstants.UPDATE_WEB_URL + "\">" + ApplicationConstants.UPDATE_WEB_URL + "</P></BODY></HTML>";
+        JEditorPane jEditorPane = new JEditorPane();
+        jEditorPane.setContentType("text/html");
+        jEditorPane.setEditable(false);
+        jEditorPane.setHighlighter(null);
+        jEditorPane.setEditable(false);
+        jEditorPane.getCaret().deinstall(jEditorPane);
+        jEditorPane.setBackground(Color.getColor("#EEEEEE"));
+        jEditorPane.addHyperlinkListener(e -> {
+            if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED)) {
+                openAppGithubPage();
+            }
+
+        });
+        jEditorPane.setText(msg);
+        JOptionPane.showMessageDialog(parentComponent,
+                                      jEditorPane,
+                                      "[WeblocOpener] " + title, JOptionPane.ERROR_MESSAGE);
+    }
+
+    public static void openAppGithubPage() {
+        if (!Desktop.isDesktopSupported()) {
+            return;
+        }
+        Desktop desktop = Desktop.getDesktop();
+
+        try {
+            desktop.browse(URI.create(ApplicationConstants.UPDATE_WEB_URL));
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(new Frame(), "URL is corrupt: " + ApplicationConstants.UPDATE_WEB_URL);
+        }
+
+    }
+}
