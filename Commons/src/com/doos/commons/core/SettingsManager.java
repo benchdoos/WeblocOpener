4c8ba1e8b79560515b4c63152683b0d7848ac871
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2016-12-05 20:50:22 +0300

    Added logging

--- Commons/src/com/doos/commons/core/SettingsManager.java
+++ Commons/src/com/doos/commons/core/SettingsManager.java
@@ -4,6 +4,9 @@ import com.doos.commons.ApplicationConstants;
 import com.doos.commons.registry.RegistryCanNotReadInfoException;
 import com.doos.commons.registry.RegistryCanNotWriteInfoException;
 import com.doos.commons.registry.RegistryManager;
+import org.apache.log4j.Logger;
+
+import static com.doos.commons.utils.Logging.getCurrentClassName;
 
 /**
  * Created by Eugene Zrazhevsky on 20.11.2016.
@@ -11,9 +14,11 @@ import com.doos.commons.registry.RegistryManager;
  * {@code SettingsManager} manages settings of application. It uses registry of Windows.
  */
 public class SettingsManager {
+    private static final Logger log = Logger.getLogger(getCurrentClassName());
 
     //You can not call this, method can be deleted
     public static void loadInfo() throws RegistryCanNotReadInfoException, RegistryCanNotWriteInfoException {
+        log.info("Loading info from registry");
         RegistryManager.getAppNameValue();
         RegistryManager.getAppVersionValue();
         RegistryManager.getInstallLocationValue();
