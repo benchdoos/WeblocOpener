4b287fcf465f19b04b0162369bd72e0105eebc62
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2016-12-05 21:14:43 +0300

    Added logging

--- UpdateModule/src/com/doos/update_module/core/Main.java
+++ UpdateModule/src/com/doos/update_module/core/Main.java
@@ -11,6 +11,7 @@ import com.doos.commons.registry.fixer.RegistryFixerAppVersionKeyFailException;
 import com.doos.commons.registry.fixer.RegistryFixerAutoUpdateKeyFailException;
 import com.doos.commons.registry.fixer.RegistryFixerInstallPathKeyFailException;
 import com.doos.commons.utils.Internal;
+import com.doos.commons.utils.Logging;
 import com.doos.commons.utils.UserUtils;
 import com.doos.commons.utils.system.SystemUtils;
 import com.doos.commons.utils.system.UnsupportedOsSystemException;
@@ -18,6 +19,7 @@ import com.doos.commons.utils.system.UnsupportedSystemVersionException;
 import com.doos.update_module.gui.UpdateDialog;
 import com.doos.update_module.nongui.NonGuiUpdater;
 import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
 
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
@@ -27,8 +29,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
 
-import static com.doos.commons.ApplicationConstants.APP_NAME;
 import static com.doos.commons.ApplicationConstants.UPDATE_PATH_FILE;
+import static com.doos.commons.ApplicationConstants.WEBLOC_OPENER_APPLICATION_NAME;
+import static com.doos.commons.utils.Logging.getCurrentClassName;
 import static com.doos.commons.utils.UserUtils.showErrorMessageToUser;
 import static com.doos.update_module.nongui.NonGuiUpdater.tray;
 import static com.doos.update_module.nongui.NonGuiUpdater.trayIcon;
@@ -39,9 +42,13 @@ import static com.doos.update_module.nongui.NonGuiUpdater.trayIcon;
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public class Main {
     public static Mode mode = Mode.NORMAL;
+    private static Logger log;
 
     public static void main(String[] args) {
         try {
+            new Logging(ApplicationConstants.UPDATER_APPLICATION_NAME);
+            log = Logger.getLogger(getCurrentClassName());
+
             SystemUtils.checkIfSystemIsSupported();
 
             enableLookAndFeel();
@@ -84,7 +91,8 @@ public class Main {
                     File[] files = new File(UPDATE_PATH_FILE).listFiles();
                     if (files != null) {
                         for (File current : files) {
-                            if (current.isFile() && current.getName().toLowerCase().contains(APP_NAME.toLowerCase()) &&
+                            if (current.isFile() && current.getName().toLowerCase().contains(
+                                    WEBLOC_OPENER_APPLICATION_NAME.toLowerCase()) &&
                                     current.getName().toLowerCase().contains("exe")) {
                                 current.delete();
                             }
@@ -112,15 +120,17 @@ public class Main {
         try {
             SettingsManager.loadInfo();
         } catch (RegistryException e) {
-            e.printStackTrace();
+            log.warn("Can not load info from registry", e);
             try {
                 RegistryFixer.fixRegistry();
             } catch (RegistryFixerAutoUpdateKeyFailException | RegistryFixerAppVersionKeyFailException e1) {
+                log.warn("Can not fix registry on startup", e1);
                 RegistryManager.setDefaultSettings();
             } catch (RegistryFixerInstallPathKeyFailException | FileNotFoundException e1) {
+                log.warn("Can not fix registry on startup", e1);
                 showErrorMessageToUser(null, "Can not fix registry",
-                                       "Registry application data is corrupt. " +
-                                               "Please re-install the " + "application.");
+                        "Registry application data is corrupt. " +
+                                "Please re-install the " + "application.");
                 System.exit(-1);
             }
 
@@ -131,8 +141,7 @@ public class Main {
      * Finds out, where app is located.
      */
     public static void initUpdateJar() {
-        final File JAR_FILE = new File(Main.class.getProtectionDomain()
-                                               .getCodeSource().getLocation().getPath());
+        final File JAR_FILE = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath());
         String property = null;
         try {
             property = RegistryManager.getInstallLocationValue();
@@ -141,8 +150,7 @@ public class Main {
         }
         File JAR_FILE_DEFAULT_LOCATION = getFileLocation(JAR_FILE, property);
 
-        System.out
-                .println("Jar: " + JAR_FILE.getAbsolutePath() + " def: " + JAR_FILE_DEFAULT_LOCATION.getAbsolutePath());
+        log.info("Jar: " + JAR_FILE.getAbsolutePath() + " def: " + JAR_FILE_DEFAULT_LOCATION.getAbsolutePath());
 
         runUpdater(JAR_FILE, JAR_FILE_DEFAULT_LOCATION);
     }
@@ -154,10 +162,10 @@ public class Main {
         if (JAR_FILE.getAbsolutePath().replace("%20", " ").equals(
                 JAR_FILE_DEFAULT_LOCATION.getAbsolutePath().replace("%20", " "))) {
             try {
-                System.out.println(
+                log.info(
                         "Creating itself at: " + new File(UPDATE_PATH_FILE + "Updater_.jar").getAbsolutePath());
                 FileUtils.copyFile(new File(JAR_FILE.getAbsolutePath().replace("%20", " ")),
-                                   new File(UPDATE_PATH_FILE + "Updater_.jar"));
+                        new File(UPDATE_PATH_FILE + "Updater_.jar"));
                 Runtime.getRuntime().exec("java -jar " + UPDATE_PATH_FILE + "Updater_.jar");
                 System.exit(0);
             } catch (IOException e) {
@@ -169,7 +177,7 @@ public class Main {
             } catch (RegistryException e) {
                 String message
                         = "Can not read data from registry.";
-                System.out.println(message);
+                log.warn(message);
                 JOptionPane.showMessageDialog(null, message, message, JOptionPane.ERROR_MESSAGE);
 
             }
@@ -181,25 +189,35 @@ public class Main {
         File JAR_FILE_DEFAULT_LOCATION;
         if (property != null) {
             JAR_FILE_DEFAULT_LOCATION = new File(property
-                                                         + File.separator + "Updater.jar");
+                    + File.separator + "Updater.jar");
         } else {
             File file = JAR_FILE.getParentFile();
-            System.out.println("Parent file is: " + file.getAbsolutePath());
-            if (file.getName().equals(ApplicationConstants.APP_NAME)) {
+            log.info("Parent file is: " + file.getAbsolutePath());
+            if (file.getName().equals(ApplicationConstants.WEBLOC_OPENER_APPLICATION_NAME)) {
                 JAR_FILE_DEFAULT_LOCATION = new File(file.getAbsolutePath() + File.separator + "Updater.jar");
             } else {
-                String programFilesPath;
-                String realArch = SystemUtils.getRealSystemArch();
-                if (realArch.equals("64")) {
-                    programFilesPath = System.getenv("ProgramFiles(X86)");
-                } else {
-                    programFilesPath = System.getenv("ProgramFiles");
-                }
+                String programFilesPath = getProgramFilesPath();
                 JAR_FILE_DEFAULT_LOCATION = new File(
                         programFilesPath + "WeblocOpener" + File.separator + "Updater.jar"); //TODO find better solution
             }
         }
         return JAR_FILE_DEFAULT_LOCATION;
+    }
+
+    /**
+     * Finds out %ProgramFiles% path
+     *
+     * @return Path to %ProgramFiles% folder
+     */
+    private static String getProgramFilesPath() {
+        String programFilesPath;
+        String realArch = SystemUtils.getRealSystemArch();
+        if (realArch.equals("64")) {
+            programFilesPath = System.getenv("ProgramFiles(X86)");
+        } else {
+            programFilesPath = System.getenv("ProgramFiles");
+        }
+        return programFilesPath;
     }
 
     private static void createUpdateDialog() {
@@ -221,7 +239,6 @@ public class Main {
                     str = ApplicationConstants.APP_VERSION;
                 }
                 if (str == null) {
-                    //str = serverAppVersion.getVersion(); //why????
                     str = ApplicationConstants.APP_VERSION;
                 } else if (str.isEmpty()) {
                     str = ApplicationConstants.APP_VERSION;
@@ -245,7 +262,9 @@ public class Main {
     private static void enableLookAndFeel() {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ignore) {/*NOP*/}
+        } catch (Exception ignore) {
+            log.warn("Could not establish UI Look and feel.");
+        }
     }
 
     public enum Mode {NORMAL, SILENT, AFTER_UPDATE, ERROR}
