d8a66b46af84425a1144a8f84bd47cbf5c9b003b
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2018-11-10 11:33:21 +0300

    Added algorithm that recognize what file is opening (in cases it's chinese or unsupported unicode(?)) Added file chooser for that cases, when there are 2 or more results

--- src/main/java/com/github/benchdoos/weblocopener/gui/UpdateInfoDialog.java
+++ src/main/java/com/github/benchdoos/weblocopener/gui/UpdateInfoDialog.java
@@ -75,7 +75,7 @@ class UpdateInfoDialog extends JDialog {
         panel2.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
         panel1.add(panel2, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         buttonOK = new JButton();
-        buttonOK.setText("OK");
+        this.$$$loadButtonText$$$(buttonOK, ResourceBundle.getBundle("spelling").getString("buttonOk"));
         panel2.add(buttonOK, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JPanel panel3 = new JPanel();
         panel3.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
@@ -86,6 +86,33 @@ class UpdateInfoDialog extends JDialog {
         textPane.setContentType(ResourceBundle.getBundle("spelling").getString("text.html"));
         textPane.setEditable(false);
         scrollPane1.setViewportView(textPane);
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    private void $$$loadButtonText$$$(AbstractButton component, String text) {
+        StringBuffer result = new StringBuffer();
+        boolean haveMnemonic = false;
+        char mnemonic = '\0';
+        int mnemonicIndex = -1;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == '&') {
+                i++;
+                if (i == text.length()) break;
+                if (!haveMnemonic && text.charAt(i) != '&') {
+                    haveMnemonic = true;
+                    mnemonic = text.charAt(i);
+                    mnemonicIndex = result.length();
+                }
+            }
+            result.append(text.charAt(i));
+        }
+        component.setText(result.toString());
+        if (haveMnemonic) {
+            component.setMnemonic(mnemonic);
+            component.setDisplayedMnemonicIndex(mnemonicIndex);
+        }
     }
 
     /**
--- src/main/java/com/github/benchdoos/weblocopener/service/Analyzer.java
+++ src/main/java/com/github/benchdoos/weblocopener/service/Analyzer.java
@@ -15,14 +15,18 @@
 
 package com.github.benchdoos.weblocopener.service;
 
+import com.github.benchdoos.weblocopener.core.constants.ApplicationConstants;
 import com.github.benchdoos.weblocopener.gui.EditDialog;
+import com.github.benchdoos.weblocopener.gui.FileChooser;
 import com.github.benchdoos.weblocopener.utils.Logging;
 import com.github.benchdoos.weblocopener.utils.UserUtils;
+import me.xdrop.fuzzywuzzy.FuzzySearch;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import static com.github.benchdoos.weblocopener.core.constants.ApplicationConstants.WEBLOC_FILE_EXTENSION;
 
@@ -47,63 +51,108 @@ public class Analyzer {
     }
 
     private void analyzeFile(String filePath) {
-        File file = null;
+        ArrayList<File> files = null;
+        File chosenFile = null;
         try {
-            file = getWeblocFile(filePath);
-            String url = "";
-            if (file != null) {
-                url = UrlsProceed.takeUrl(file);
+            files = getWeblocFile(filePath);
+            String url;
+            if (files != null) {
+                if (files.size() == 1) {
+                    chosenFile = files.get(0);
+                    url = UrlsProceed.takeUrl(chosenFile);
+                    if (url.isEmpty()) {
+                        throw new NullPointerException("Url is empty, just editing");
+                    } else {
+                        this.url = url;
+                    }
+                } else {
+                    FileChooser fileChooser = new FileChooser(files);
+                    fileChooser.setVisible(true);
+                    chosenFile = fileChooser.getChosenFile();
+                    log.debug("Chosen file is: {}", chosenFile);
+                    if (chosenFile != null) {
+                        url = UrlsProceed.takeUrl(chosenFile);
+                        if (url.isEmpty()) {
+                            new EditDialog(chosenFile.getAbsolutePath()).setVisible(true);
+                        } else {
+                            this.url = url;
+                        }
+                    }
+                }
             }
-            if (url.isEmpty()) {
-                throw new NullPointerException("Url is empty, just editing");
-            }
-            this.url = url;
+
         } catch (NullPointerException e) {
-            log.warn("URL in file [" + file + "] has empty link.", e);
-            if (file != null) {
-                new EditDialog(file.getAbsolutePath()).setVisible(true);
-            }
+            log.warn("URL in files [" + files + "] has empty link.", e);
+            editFile(files);
         } catch (IOException e) {
-            String message = "Can not read file [" + file + "]";
+            String message = "Can not read files [" + files + "]";
             log.warn(message, e);
             UserUtils.showErrorMessageToUser(null, "Error", message);
         } catch (Exception e) {
-            final String message = "URL in file [" + file + "] is corrupt.";
+            final String message = "URL in files [" + files + "] is corrupt.";
             log.warn(message, e);
             UserUtils.showErrorMessageToUser(null, "Error", message);
-            if (file != null) {
-                new EditDialog(file.getAbsolutePath()).setVisible(true);
+
+            if (chosenFile != null) {
+                new EditDialog(chosenFile.getAbsolutePath()).setVisible(true);
             }
         }
     }
 
+    private int compareFileNames(File fileOriginal, File fileComparing) {
+        final String originalFileName = fileOriginal.getName();
+        final String comparingFileName = fileComparing.getName();
+        return FuzzySearch.ratio(originalFileName, comparingFileName);
+    }
 
-    /**
-     * Returns a <code>.webloc</code> file from path.
-     *
-     * @param arg File path.
-     * @return <code>.webloc</code> file.
-     */
-    private File getWeblocFile(String arg) {
-
-        File currentFile = new File(arg);
-        log.info("File [" + arg + "] exists: " + currentFile.exists() + " file?: " + currentFile.isFile());
-        if (currentFile.isFile() && currentFile.exists()) {
-            if (getFileExtension(currentFile).equals(WEBLOC_FILE_EXTENSION)) {
-                log.info("File added to proceed: " + currentFile.getAbsolutePath());
-                return currentFile;
+    private void editFile(ArrayList<File> files) {
+        if (files != null) {
+            if (files.size() == 1) {
+                new EditDialog(files.get(0).getAbsolutePath()).setVisible(true);
             } else {
-                log.warn("Wrong argument. File extension is not webloc: " + currentFile.getAbsolutePath());
+                FileChooser fileChooser = new FileChooser(files);
+                fileChooser.setVisible(true);
+                final File chosenFile = fileChooser.getChosenFile();
+                if (chosenFile != null) {
+                    new EditDialog(chosenFile.getAbsolutePath()).setVisible(true);
+                }
             }
-        } else {
-            log.warn("Wrong argument. Invalid file path or file not exist: " + currentFile.getAbsolutePath());
         }
+    }
 
+    private ArrayList<File> findOpeningFile(File currentFile) {
+        File parent = currentFile.getParentFile();
+        if (parent.exists() && parent.isDirectory()) {
+            ArrayList<ComparedFile> values = new ArrayList<>();
+
+            final File[] files = parent.listFiles();
+            if (files != null) {
+                for (File current : files) {
+                    if (getFileExtension(current).equalsIgnoreCase(ApplicationConstants.WEBLOC_FILE_EXTENSION)) {
+                        int compared = compareFileNames(currentFile, current);
+                        values.add(new ComparedFile(compared, current));
+                    }
+                }
+
+                final int maximumValue = getMaximumValue(values);
+
+                return getAllFilesWithMaximumValue(maximumValue, values);
+            }
+        }
         return null;
     }
 
-    public String getUrl() {
-        return url;
+    private ArrayList<File> getAllFilesWithMaximumValue(int maximumValue, ArrayList<ComparedFile> values) {
+        ArrayList<File> result = null;
+        for (ComparedFile c : values) {
+            if (c.getEqualSymbols() == maximumValue) {
+                if (result == null) {
+                    result = new ArrayList<>();
+                }
+                result.add(c.getFile());
+            }
+        }
+        return result;
     }
 
     /**
@@ -127,6 +176,85 @@ public class Analyzer {
             return name.substring(name.lastIndexOf(".") + 1);
         } catch (Exception e) {
             return "";
+        }
+    }
+
+    private int getMaximumValue(ArrayList<ComparedFile> values) {
+        int result = 0;
+        for (ComparedFile comparedFile : values) {
+            if (comparedFile.getEqualSymbols() > result) {
+                result = comparedFile.getEqualSymbols();
+            }
+        }
+        return result;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * Returns a <code>.webloc</code> file from path.
+     *
+     * @param arg File path.
+     * @return <code>.webloc</code> file.
+     */
+    private ArrayList<File> getWeblocFile(String arg) {
+
+        File currentFile = new File(arg);
+        log.info("File [" + arg + "] exists: " + currentFile.exists() + " file?: " + currentFile.isFile());
+        if (currentFile.isFile() && currentFile.exists()) {
+            if (getFileExtension(currentFile).equals(WEBLOC_FILE_EXTENSION)) {
+                log.info("File added to proceed: " + currentFile.getAbsolutePath());
+                ArrayList<File> arrayList = new ArrayList<>();
+                arrayList.add(currentFile);
+                return arrayList;
+            } else {
+                log.warn("Wrong argument. File extension is not webloc: " + currentFile.getAbsolutePath());
+            }
+        } else {
+            log.warn("Wrong argument. Invalid file path or file not exist: " + currentFile.getAbsolutePath());
+            log.info("Trying to guess what file user wants to open");
+            ArrayList<File> files = findOpeningFile(currentFile);
+            log.info("Got files, that match: {}", files);
+            return files;
+        }
+
+        return null;
+    }
+
+    private class ComparedFile {
+        private int equalSymbols;
+        private File file;
+
+        public ComparedFile(int equalSymbols, File file) {
+            this.equalSymbols = equalSymbols;
+            this.file = file;
+        }
+
+        public int getEqualSymbols() {
+            return equalSymbols;
+        }
+
+        public ComparedFile setEqualSymbols(int equalSymbols) {
+            this.equalSymbols = equalSymbols;
+            return this;
+        }
+
+        public File getFile() {
+            return file;
+        }
+
+        public ComparedFile setFile(File file) {
+            this.file = file;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return "ComparedFile{" + "equalSymbols=" + equalSymbols +
+                    ", file=" + file +
+                    '}';
         }
     }
 }
