19798f36bc1f5b2f9c17bc9375075e658a5d2ef9
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2019-02-08 21:23:22 +0300

    Added DesktopEntryLink for .desktop icon for linux

---
+++ src/main/java/com/github/benchdoos/weblocopener/service/links/DesktopEntryLink.java
@@ -0,0 +1,48 @@
+/*
+ * (C) Copyright 2019.  Eugene Zrazhevsky and others.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * Contributors:
+ * Eugene Zrazhevsky <eugene.zrazhevsky@gmail.com>
+ */
+
+package com.github.benchdoos.weblocopener.service.links;
+
+import com.github.benchdoos.weblocopener.core.constants.ApplicationConstants;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+
+public class DesktopEntryLink implements AbstractLink {
+    //[Desktop Entry]
+    //Encoding=UTF-8
+    //Name=Link to your site
+    //URL=http://your-site-url.com
+    //Icon=text-html
+
+    @Override
+    public void createLink(File file, URL url) throws IOException {
+        FileWriter writer = new FileWriter(file);
+        writer.write("[Desktop Entry]\n");
+        writer.write("Encoding=" + ApplicationConstants.DEFAULT_APPLICATION_CHARSET + "\n");
+        writer.write("Name=" + file.getName() + "\n");
+        writer.write("URL=" + url.toString() + "\n");
+        writer.write("Icon=text-html" + "\n");
+        writer.flush();
+        writer.close();
+    }
+
+    @Override
+    public URL getLink(File file) throws IOException {
+        return LinkUtils.getUrl(file);
+    }
+}
