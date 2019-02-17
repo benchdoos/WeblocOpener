c0307ae17d3947ea179d5e8a025de788a36a1c2f
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2019-02-17 12:37:57 +0300

    Updated visibility

--- src/main/java/com/github/benchdoos/weblocopener/gui/PlaceholderTextField.java
+++ src/main/java/com/github/benchdoos/weblocopener/gui/PlaceholderTextField.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2018.  Eugene Zrazhevsky and others.
+ * (C) Copyright 2019.  Eugene Zrazhevsky and others.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -20,7 +20,7 @@ import javax.swing.text.Document;
 import java.awt.*;
 
 @SuppressWarnings("serial")
-class PlaceholderTextField extends JTextField {
+public class PlaceholderTextField extends JTextField {
 
     private String placeholder;
 
