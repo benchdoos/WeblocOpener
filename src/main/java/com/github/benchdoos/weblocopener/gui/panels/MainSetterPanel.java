8450447032c2da6ace8acd917f742cd3049fda6a
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2019-04-25 15:19:25 +0300

    Updated UI

--- src/main/java/com/github/benchdoos/weblocopener/gui/panels/MainSetterPanel.java
+++ src/main/java/com/github/benchdoos/weblocopener/gui/panels/MainSetterPanel.java
@@ -104,7 +104,7 @@ public class MainSetterPanel extends JPanel implements SettingsPanel, Translatab
         panel2.add(spacer2, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         unixOpenModePanel = new JPanel();
         unixOpenModePanel.setLayout(new GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1));
-        panel1.add(unixOpenModePanel, new GridConstraints(4, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        panel1.add(unixOpenModePanel, new GridConstraints(4, 0, 1, 3, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         unixOpenModePanel.setBorder(BorderFactory.createTitledBorder(ResourceBundle.getBundle("translations/MainSetterPanelBundle").getString("onlyForUnix")));
         final JLabel label1 = new JLabel();
         this.$$$loadLabelText$$$(label1, ResourceBundle.getBundle("translations/MainSetterPanelBundle").getString("unixOpenModeLabel"));
