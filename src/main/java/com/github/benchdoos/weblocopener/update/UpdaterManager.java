cb0901a48c90eeeb68a5fc9a4f07f21f3d7bc4e5
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2019-07-21 14:13:24 +0300

    Added new getting of server beta latest ApplicationVersion

--- src/main/java/com/github/benchdoos/weblocopener/update/UpdaterManager.java
+++ src/main/java/com/github/benchdoos/weblocopener/update/UpdaterManager.java
@@ -21,22 +21,14 @@ import com.github.benchdoos.weblocopener.utils.Logging;
 import com.github.benchdoos.weblocopener.utils.system.OperatingSystem;
 import com.github.benchdoos.weblocopener.utils.version.ApplicationVersion;
 import com.github.benchdoos.weblocopener.utils.version.Beta;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jetbrains.annotations.NotNull;
 import org.kohsuke.github.GHRelease;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
+import org.kohsuke.github.PagedIterable;
 
-import javax.net.ssl.HttpsURLConnection;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -45,8 +37,6 @@ public class UpdaterManager {
     private static final String REPOSITORY_NAME = "benchdoos/weblocopener";
 
     private static final Logger log = LogManager.getLogger(Logging.getCurrentClassName());
-    private static final int CONNECTION_TIMEOUT = 500;
-    private static final String DEFAULT_ENCODING = "UTF-8";
     private static final Pattern BETA_FROM_RELEASE_TITLE_PATTERN = Pattern.compile("\\(beta\\.(\\d+)\\)");
 
     static ApplicationVersion getLatestVersion(Updater updater) {
@@ -80,29 +70,6 @@ public class UpdaterManager {
         }
     }
 
-    private static HttpsURLConnection createConnection(@NotNull String urlString) throws IOException {
-        try {
-            HttpsURLConnection connection;
-            log.debug("Creating connection to github: {}", urlString);
-
-            URL url = new URL(urlString);
-
-            connection = (HttpsURLConnection) url.openConnection();
-            connection.setConnectTimeout(CONNECTION_TIMEOUT);
-
-            if (!connection.getDoOutput()) {
-                connection.setDoOutput(true);
-            }
-            if (!connection.getDoInput()) {
-                connection.setDoInput(true);
-            }
-            return connection;
-        } catch (IOException e) {
-            log.warn("Could not establish connection to github: {}", urlString, e);
-            throw new IOException(e);
-        }
-    }
-
     public static Updater getUpdaterForCurrentOperatingSystem() {
         if (OperatingSystem.isWindows()) {
             return new WindowsUpdater();
@@ -111,73 +78,27 @@ public class UpdaterManager {
         } else return null;
     }
 
-    private static ApplicationVersion formAppVersionFromLatestReleaseJson(String setupName, JsonObject root) {
-        final String version = "tag_name";
-        final String browser_download_url = "browser_download_url";
-        final String assets = "assets";
-        final String name = "name";
-        final String size = "size";
-        final String info = "body";
-        final String prerelease = "prerelease"; //beta if true
-
-        ApplicationVersion applicationVersion = new ApplicationVersion();
-
-
-        applicationVersion.setVersion(root.getAsJsonObject().get(version).getAsString());
-        applicationVersion.setUpdateInfo(root.getAsJsonObject().get(info).getAsString());
-        applicationVersion.setUpdateTitle(root.getAsJsonObject().get(name).getAsString());
-        final boolean isPreRelease = root.getAsJsonObject().get(prerelease).getAsBoolean();
-        applicationVersion.setBeta(tryGetBetaFromName(applicationVersion.getUpdateTitle(), new Beta(isPreRelease ? 1 : 0)));
-
-        JsonArray asserts = root.getAsJsonArray(assets);
-        for (JsonElement assert_ : asserts) {
-            JsonObject userObject = assert_.getAsJsonObject();
-            if (userObject.get(name).getAsString().equals(setupName)) {
-                applicationVersion.setDownloadUrl(userObject.get(browser_download_url).getAsString());
-                applicationVersion.setSize(userObject.get(size).getAsLong());
-            }
-        }
-        return applicationVersion;
-    }
-
     static ApplicationVersion getLatestBetaVersion(String setupName) {
         try {
-            final HttpsURLConnection connection = UpdaterManager.createConnection(Updater.ALL_RELEASES_URL);
-            final ApplicationVersion serverLatestBetaApplicationVersion = getLatestBetaAppVersion(setupName, connection);
-
-            log.info("Server latest beta application version: {}", serverLatestBetaApplicationVersion);
-
-            return serverLatestBetaApplicationVersion;
+            final GitHub gitHub = GitHub.connectAnonymously();
+            final GHRepository repository = gitHub.getRepository(REPOSITORY_NAME);
+            final PagedIterable<GHRelease> ghReleases = repository.listReleases();
+            return getLatestBetaAppVersion(ghReleases, setupName);
         } catch (IOException e) {
             log.warn("Can not get latest beta application version", e);
             return null;
         }
     }
 
-    private static ApplicationVersion formAppVersionFromAllReleasesJson(String setupName, JsonArray array) {
+    private static ApplicationVersion getLatestBetaAppVersion(PagedIterable<GHRelease> releases, String setupName) throws IOException {
         ApplicationVersion latestBeta = null;
-
-        for (JsonElement jsonElement : array) {
-            final ApplicationVersion applicationVersion = formAppVersionFromLatestReleaseJson(setupName, jsonElement.getAsJsonObject());
-            if (applicationVersion.isBeta()) {
-                latestBeta = applicationVersion;
+        for (GHRelease release : releases) {
+            if (release.isPrerelease()) {
+                latestBeta = getApplicationVersion(release, setupName);
                 break;
             }
         }
         return latestBeta;
-    }
-
-    private static ApplicationVersion getLatestBetaAppVersion(String setupName, HttpsURLConnection connection) throws IOException {
-        log.debug("Getting latest server beta application version");
-        String input;
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), DEFAULT_ENCODING));
-
-        input = bufferedReader.readLine();
-
-        JsonParser parser = new JsonParser();
-        JsonArray root = parser.parse(input).getAsJsonArray();
-
-        return UpdaterManager.formAppVersionFromAllReleasesJson(setupName, root);
     }
 
     private static ApplicationVersion getApplicationVersion(GHRelease latestRelease, String setupName) throws IOException {
