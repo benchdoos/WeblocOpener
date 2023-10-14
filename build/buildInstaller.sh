b5a85293debc001459c0ea15b47137f3e001d79d
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2023-10-14 21:59:23 +0300

    hotfix/maven_build: Added logs

--- build/buildInstaller.sh
+++ build/buildInstaller.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+echo current location: $PWD
+ls -l $PWD
+
 #exec docker run --rm -i -v "$PWD:/work" amake/innosetup "$@"
 exec docker run --rm -i -v "$PWD/../:/work" amake/innosetup build/WeblocOpener.iss
