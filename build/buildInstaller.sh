75bda381ae872579e0d8513040dbe08578363330
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2023-10-14 22:34:11 +0300

    hotfix/maven_build: Some improvements

--- build/buildInstaller.sh
+++ build/buildInstaller.sh
@@ -5,15 +5,17 @@ echo root location: $GITHUB_WORKSPACE
 ls -l $PWD
 
 if [ -n "$GITHUB_WORKSPACE" ]; then
-    working_location="$GITHUB_WORKSPACE"
+    working_location=$GITHUB_WORKSPACE
 else
-    working_location="$PWD"
+    working_location=$PWD
 fi
 
 echo "Working Location: $working_location"
 cd $working_location
 echo current location: $PWD
 ls -a
+
+chmod 777 $working_location/target
 
 #exec docker run --rm -i -v "$PWD:/work" amake/innosetup "$@"
 exec docker run --rm -i -v "$working_location/WeblocOpener/../:/work" amake/innosetup build/WeblocOpener.iss
